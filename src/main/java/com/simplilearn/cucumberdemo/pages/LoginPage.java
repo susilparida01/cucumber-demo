cdfdac65a5d9cf66fbe065913e6d63366a62ae6c
Susil Parida <susil.parida01@gmail.com>
2026-08-23 22:48:36 +0530

    Add WebDriverWait initialization in LoginPage

--- src/main/java/com/simplilearn/cucumberdemo/pages/LoginPage.java
+++ src/main/java/com/simplilearn/cucumberdemo/pages/LoginPage.java
@@ -24,6 +24,7 @@ public class LoginPage {
 	
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 	
 	public void enterUsername(String username) {
